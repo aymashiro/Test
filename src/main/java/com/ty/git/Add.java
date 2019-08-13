b4234fda250801d4b2b0b32c195549928aeaa908
tom_glb <goodMorning@qq.com>
2019-08-13 21:13:33 +0800

    new edit

--- src/main/java/com/ty/git/Add.java
+++ src/main/java/com/ty/git/Add.java
@@ -4,7 +4,7 @@ public class Add {
 
 	public static void main(String[] args) {
 		System.out.println("Test..aweaeeaw.");
-		System.out.println("hot fix....");
+		System.out.println("hot fix..22222..");
 		
 	}
 }
